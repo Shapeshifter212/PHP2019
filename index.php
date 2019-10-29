1c5c10f36ce0a3d4b85bd6f4f162b8eb956d97ee
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-10-29 16:06:15 +0200

    Antra užduotis

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Aš Valdau HTML!</title>
+    <title>Aš Valdau HTML ir PHP!</title>
   </head>
   <body>
-    <h1><b>Justas</b> - HTML asas!</h1>
-    <p>Moku kurti statinį turinį.</p>
+    <h1><b>Justas</b> - HTML ir PHP asas!</h1>
+    <p><font color="grey">Moku kurti statinį turinį.</font></p>
+    <p><font color="grey">Atlikta: </font></p> 
+    <p><font color="blue"> 
+        <?php echo date("Y/m/d h:m:s");
+        ?>
+        </font>
+    </p>  
   </body>
 </html>
