cb700798d7e1b24daee9a42ed23092d55ba6cdaf
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-10-29 15:07:12 +0200

    Pirma užduotis

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php print 'he';
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aš Valdau HTML!</title>
+  </head>
+  <body>
+    <h1>HTML asas!</h1>
+    <p>Moku kurti statinį turinį.</p>
+  </body>
+</html>
