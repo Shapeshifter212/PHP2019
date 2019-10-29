9de749035fd9d5895aa8ab68caf0d55e0f39ad8f
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-10-29 16:18:24 +0200

    Antra užduotis updated

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Aš Valdau HTML ir PHP!</title>
-  </head>
-  <body>
-    <h1><b>Justas</b> - HTML ir PHP asas!</h1>
-    <p><font color="grey">Moku kurti statinį turinį.</font></p>
-    <p><font color="grey">Atlikta: </font></p> 
-    <p><font color="blue"> 
-        <?php echo date("Y/m/d h:m:s");
-        ?>
-        </font>
-    </p>  
-  </body>
-</html>
+ <html lang="en" dir="ltr">
+   <head>
+     <title>Aš valdau HTML <?php print 'ir PHP!'; ?></title>
+   </head>
+   <body>
+       <h1><b>Justas</b> - HTML <?php print 'ir PHP'; ?> asas!</h1>
+       <p>
+           Kuriu dinaminį turinį.<br>
+           Atidaryta: <?php print date('Y-m-d H:i:s'); ?>
+       </p>
+   </body>
+ </html> 
