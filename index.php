fefeb11cfd183f0727596e222079199a1e5b2f04
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-10-29 17:15:44 +0200

    Ketvirta užduotis

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <html lang="en" dir="ltr">
     <head>
-        <title><?php print 'Aš,' . date('l') . 'ir PHP'; ?></title>
+        <title><?php print 'PHP lydės ir ' . date('Y-m-d', strtotime('+1 day')); ?></title>
     </head>
     <body>
         <h1>
-            <b>Justas</b> - HTML <?php print 'ir PHP'; ?> asas jau nuo <?php print date('Y') . ' metų'; ?>
+            <b>Justas</b> - HTML <?php print 'PHP su manim buvo ' . date('h:i:s', strtotime('-1 hour')); ?>
         </h1>
         <p> 
-            Viskas prasidejo <?php print date('m') . ' menesio ' . date('d') . ' diena'; ?> 
+            <?php print date('y-m-d', strtotime('+1 year')) . ' ne už kalnų!'; ?> 
         </p>
     </body>
 </html> 
