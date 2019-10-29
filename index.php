e328d012e45e4aaacb45c8aefe79ac748d8d50f7
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-10-29 16:48:10 +0200

    Trečia užduotis updated

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
- <html lang="en" dir="ltr">
-   <head>
-     <title>Aš,<?php print date('l'); ?> ir PHP</title>
-   </head>
-   <body>
-       <h1><b>Justas</b> - HTML <?php print 'ir PHP'; ?> asas jau nuo <?php print date('o'); ?></h1>
-       <p>
-           Viskas prasidėjo <?php print date('F'); ?> mėnesio <?php print date('d'); ?> dieną
-       </p>
-   </body>
- </html> 
+<html lang="en" dir="ltr">
+    <head>
+        <title>Aš,<?php print date('l'); ?> ir PHP</title>
+    </head>
+    <body>
+        <h1><b>Justas</b> - HTML <?php print 'ir PHP'; ?> asas jau nuo <?php print date('o'); ?></h1>
+        <p> Viskas prasidejo <?php print date('m') . ' menesio ' . date('d') . ' diena'; ?> 
+        </p>
+    </body>
+</html> 
