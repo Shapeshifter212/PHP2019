294c7b5a995c632a075b6982b83e06f9e0971a64
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-10-30 15:13:21 +0200

    8 užduotis upd

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
     <head>
         <meta charset="utf-8">
         <title>8 užduotis</title>
+        <style>
+           body {background: rgb <?php print rand(0,255) . ',' . rand(0,255) . ',' . rand(0,255);?>} 
+           h1 {font-size: <?php print rand(1, 50).'px'; ?>}
+           p {color: rgb <?php print rand(0,255).', '.rand(0,255).', '.rand(0,255);?>}
+        </style>
     </head>
-    <body style="background: <?php print 'rgb('.rand(0,255).', '.rand(0,255).', '.rand(0,255).', 0.73)'; ?>">
-        <h1 style="font-size: <?php print rand(1, 50); ?>">Aš keičiu savo dydį!</h1>
-        <p style="color: <?php print 'rgb('.rand(0,255).', '.rand(0,255).', '.rand(0,255).', 0.73)'; ?>">Aš keičiu savo spalvą!</p>
+    <body>
+        <h1>Aš keičiu savo dydį!</h1>
+        <p>Aš keičiu savo spalvą!</p>
     </body>
 </html>
