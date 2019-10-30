1bce6f944a44f6dd1509e049d89e5d348305ff62
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-10-30 14:42:45 +0200

    8 užduotis

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>8 užduotis</title>
-  </head>
-  <body style="background-color: rgb(<?= implode(",", $rgbColor); ?>);">
-      <h1 style="font-size: <?php rand (); ?>">Aš keičiu savo dydį!</h1>
-      <p style="color: rgb(201, 76, 76)">A6 keičiu savo spalvą!</p>
-  </body>
+    <head>
+        <meta charset="utf-8">
+        <title>8 užduotis</title>
+    </head>
+    <body style="background: <?php print 'rgb('.rand(0,255).', '.rand(0,255).', '.rand(0,255).', 0.73)'; ?>">
+        <h1 style="font-size: <?php print rand(1, 50); ?>">Aš keičiu savo dydį!</h1>
+        <p style="color: <?php print 'rgb('.rand(0,255).', '.rand(0,255).', '.rand(0,255).', 0.73)'; ?>">Aš keičiu savo spalvą!</p>
+    </body>
 </html>
