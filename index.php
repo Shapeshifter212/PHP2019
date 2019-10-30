57bfe13d640e79ceb4d4ba28a80ac4e8b12e2f6e
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-10-30 13:43:36 +0200

    8 užduotis

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
-<html>
-    <title>Komentarai</title>
-    <body>
-        <h1>
-            Dainų tekstai
-        </h1>
-        <h2>
-            Paragrafo Daina
-        </h2>
-        <p>Aš toks jaunas paragrafas....</p>
-        <!--
-        <p>Mindau svetimo....</p>
-    -->
-        </body>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>8 užduotis</title>
+  </head>
+  <body style="background-color: rgb(<?= implode(",", $rgbColor); ?>);">
+      <h1 style="font-size: <?php rand (); ?>">Aš keičiu savo dydį!</h1>
+      <p style="color: rgb(201, 76, 76)">A6 keičiu savo spalvą!</p>
+  </body>
 </html>
