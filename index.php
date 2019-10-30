85bea52af8bb8b6df0b024249ccebaab3b60ce4b
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-10-30 15:33:10 +0200

    9b

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>8 užduotis</title>
+        <title>9b užduotis</title>
         <style>
-           body {background: rgb <?php print rand(0,255) . ',' . rand(0,255) . ',' . rand(0,255);?>} 
-           h1 {font-size: <?php print rand(1, 50).'px'; ?>}
-           p {color: rgb <?php print rand(0,255).', '.rand(0,255).', '.rand(0,255);?>}
+            .imag-1 {background-image: url(https://upload.wikimedia.org/wikipedia/commons/2/2c/Alea_1.png);}
+            .imag-2 {background-image: url(https://upload.wikimedia.org/wikipedia/commons/b/b8/Alea_2.png);}  
+            .imag-3 {background-image: url(https://upload.wikimedia.org/wikipedia/commons/2/2f/Alea_3.png);}  
+            .imag-4 {background-image: url(https://upload.wikimedia.org/wikipedia/commons/8/8d/Alea_4.png);}  
+            .imag-5 {background-image: url(https://upload.wikimedia.org/wikipedia/commons/5/55/Alea_5.png);}  
+            .imag-6 {background-image: url(https://upload.wikimedia.org/wikipedia/commons/f/f4/Alea_6.png);}  
+            .kaulas {
+                height: 112px;
+                width:  112px;
+            }
         </style>
     </head>
     <body>
-        <h1>Aš keičiu savo dydį!</h1>
-        <p>Aš keičiu savo spalvą!</p>
+        <div class="kaulas <?php print 'imag-' . rand(1, 6); ?>">
+
+        </div>
     </body>
 </html>
