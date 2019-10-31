cf4c06e5f1657f164b4a40b0f4f56e1526ad4526
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-10-31 15:33:47 +0200

    II uzduotis

--- index.php
+++ index.php
@@ -1,26 +1,16 @@
 <?php
-$string = 'Opapa';
-$int = 3;
-$bool = true;
-$null = null;
-
-$li_1 = $string;
-$li_2 = $int;
-$li_3 = $bool;
-$li_4 = $null;
+$img = 'https://i.pinimg.com/originals/7a/f7/de/7af7de3affa1761fe56d9d616f022db6.jpg';
 ?>
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>I užduotis</title>
+        <title>II užduotis</title>
         <style></style>
     </head>
     <body>
-        <ul>
-            <li>String tipas: <?php echo $li_1 ?></li>
-            <li>Integer tipas: <?php echo $li_2 ?></li>
-            <li>Boolen tipas: <?php echo $li_3 ?></li>
-            <li>Null tipas: <?php echo $li_4 ?></li>
-        </ul>
+        <img class=foto src="<?php echo $img;?>">
+        <img class=foto src="<?php echo $img;?>">
+        <img class=foto src="<?php echo $img;?>">
+        <img class=foto src="<?php echo $img;?>">
     </body>
 </html>
