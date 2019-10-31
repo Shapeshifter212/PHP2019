d734f926e515fa8b8229c59e809772f408a6cdb3
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-10-31 15:15:58 +0200

    I uzduotis upd

--- index.php
+++ index.php
@@ -3,6 +3,11 @@ $string = 'Opapa';
 $int = 3;
 $bool = true;
 $null = null;
+
+$li_1 = $string;
+$li_2 = $int;
+$li_3 = $bool;
+$li_4 = $null;
 ?>
 <html lang="en" dir="ltr">
     <head>
@@ -12,10 +17,10 @@ $null = null;
     </head>
     <body>
         <ul>
-            <li>String tipas: <?php echo $string ?></li>
-            <li>Integer tipas: <?php echo $int ?></li>
-            <li>Boolen tipas: <?php echo $bool ?></li>
-            <li>Null tipas: <?php echo $null ?></li>
+            <li>String tipas: <?php echo $li_1 ?></li>
+            <li>Integer tipas: <?php echo $li_2 ?></li>
+            <li>Boolen tipas: <?php echo $li_3 ?></li>
+            <li>Null tipas: <?php echo $li_4 ?></li>
         </ul>
     </body>
 </html>
