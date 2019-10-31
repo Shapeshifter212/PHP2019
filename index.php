04896e09e5167d9dcb5a7a4ec1fd77e8ad8f35e6
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-10-31 16:49:34 +0200

    IV uzduotis

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <?php
+$bin_vol = 40;
+$bin_heap_vol = rand(1, 20);
+$trash_per_day = 15;
+$days = floor(($bin_vol + $bin_heap_vol) / $trash_per_day);
 
-$h3_1 = rand(101, 200);
-$h3_2 = rand(201, 300);
-$h3_3 = rand(301, 400);
-
+$h1 = 'Siuksliu prognoze';
+$p_1 = "Turima siuksline $bin_vol litru";
+$p_2 = "Zmona nesako, kol kaupas nevirsija $bin_heap_vol";
+$h3 = "Isvada: Nieko nedarysiu $days";
 ?>
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>III užduotis</title>
+        <title>IV užduotis</title>
         <style></style>
     </head>
     <body>
-        <h1>Skolos skaiciuokle</h1>
-        <h3>Jei paeimei <?php print $h3_1; ?> jievru</h3>
-        <h3>Su dviem kabanciai grazinsi <?php print $h3_3; ?></h3>
-        <h3>Su vienu kabanciu grazinsi <?php print $h3_2; ?></h3>  
+        <h1><?php print $h1; ?></h1>
+        <p><?php print $p_1; ?></p>
+        <p><?php print $p_2; ?></p>
+        <h3><?php print $h3; ?></h3>  
     </body>
 </html>
