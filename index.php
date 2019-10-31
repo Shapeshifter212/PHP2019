bbc649f4c363637708b8070ba0684a1071ee4370
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-10-31 15:56:37 +0200

    III uzduotis

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php
-$img = 'https://i.pinimg.com/originals/7a/f7/de/7af7de3affa1761fe56d9d616f022db6.jpg';
+
+$h3_1 = rand(101, 200);
+$h3_2 = rand(201, 300);
+$h3_3 = rand(301, 400);
+
 ?>
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>II užduotis</title>
+        <title>III užduotis</title>
         <style></style>
     </head>
     <body>
-        <img class=foto src="<?php echo $img;?>">
-        <img class=foto src="<?php echo $img;?>">
-        <img class=foto src="<?php echo $img;?>">
-        <img class=foto src="<?php echo $img;?>">
+        <h1>Skolos skaiciuokle</h1>
+        <h3>Jei paeimei <?php print $h3_1; ?> jievru</h3>
+        <h3>Su dviem kabanciai grazinsi <?php print $h3_3; ?></h3>
+        <h3>Su vienu kabanciu grazinsi <?php print $h3_2; ?></h3>  
     </body>
 </html>
