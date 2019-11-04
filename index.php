d4d639d622285d6a9c8e3b2315a4e4709bc17612
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-11-04 19:52:39 +0200

    7 uzduotis error

--- index.php
+++ index.php
@@ -1,39 +1,38 @@
 <?php
-$sunny = rand(0, 1);
+$bool = true;
+$str = '1';
+$flt = 1.23;
+$int = 1;
 ​
-if ($sunny) {
-    $h1 = 'Sauleta';
-    $class = 'bg-sunny';
+if ($bool == $int) {
+    //       if ($bool === $int) {          } else { };
+    $bool_int = $bool === $int ? 'Identiski' : 'Lygus';
 } else {
-    $h1 = 'Debesuota';
-    $class = 'bg-cloudy';
+    $bool_int = 'Nelygus';
 }
+​
+if ($str == $bool) {
+    $str_bool = $str === $bool ? 'Identiski' : 'Lygus';
+} else {
+    $str_bool = 'Nelygus';
+}
+​
+if ($flt == $str) {
+    $flt_str = $flt === $str ? 'Identiski' : 'Lygus';
+} else {
+    $flt_str = 'Nelygus';
+}
+​
+$li_1 = "Bool (true) ir Integer (1): $bool_int";
+$li_2 = "String (1) ir Boolean (1): $str_bool";
+$li_3 = "Float (1.23) ir string(1): $flt_str";
 ?>
-<html>
-    <head>
-        <style>
-            .content {
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-            }
-            .bg-img {
-                width: 150px;
-                height: 150px;
-                background-size: cover;
-            }
-            .bg-cloudy {
-                background-image: url("https://icon-library.net/images/cloud-icon-png-transparent/cloud-icon-png-transparent-0.jpg")
-            }
-            .bg-sunny {
-                background-image: url("https://images.vexels.com/media/users/3/145134/isolated/preview/46b65a02ff99e7bb4e84d4d3d627a729-sun-sharp-beams-icon-by-vexels.png");
-            }
-        </style>
-    </head>
-    <body>
-        <div class="content">
-            <div class="bg-img <?php print $class; ?>"></div>
-            <h1><?php print $h1; ?></h1>
-        </div>
+<html lang="en">
+    <body> 
+        <ul>
+            <li><?php print $li_1; ?></li>
+            <li><?php print $li_2; ?></li>
+            <li><?php print $li_3; ?></li>
+        </ul>
     </body>
 </html>
