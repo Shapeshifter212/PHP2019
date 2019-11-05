beb57caac85a2048824bea1d09313931603014a6
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-11-05 14:59:54 +0200

    2 ciklas

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
-$x = rand (0, 23);
-for ($y = 0; $y < $x; $y++) {
-    var_dump("Tai yras $x-asis ciklas");
+$kates = rand(1, 3);
+$sunys = rand(1, 3);
+$pavyko = rand(0, 3);
+$katasuniai = 0;
+
+for ($z = 0; $z < $kates; $z++) {
+    for ($x = 0; $x < $sunys; $x++) {
+        $pavyko = rand(0, 1);
+        if ($pavyko) {
+            $katasuniai++;
+            break;
+        }
+    }
 }
+
+$h1 = 'Katašunių išeiga';
+$h2 = "Dalyvavo $kates katės ir $sunys šunys";
+$h3 = "Katašunių išeiga: $katasuniai";
+?>
+<html>
+    <head>
+        <title>2 ciklas</title>
+    </head>
+    <body>
+        <h1><?php echo $h1; ?></h1>
+        <h2><?php echo $h2; ?></h2>
+        <h3><?php echo $h3; ?></h3>
+    </body>
+</html>
