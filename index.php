7c0dca77e21734ef4b6cbbd67a1d806969e86b8c
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-11-05 15:20:33 +0200

    2 ciklas comment

--- index.php
+++ index.php
@@ -4,12 +4,12 @@ $sunys = rand(1, 3);
 $pavyko = rand(0, 3);
 $katasuniai = 0;
 
-for ($z = 0; $z < $kates; $z++) {
-    for ($x = 0; $x < $sunys; $x++) {
-        $pavyko = rand(0, 1);
-        if ($pavyko) {
+for ($z = 0; $z < $kates; $z++) {       // ima kates ir tikrina su sunim
+    for ($x = 0; $x < $sunys; $x++) {   // tikrina suns pajegumus
+        $pavyko = rand(0, 1);           // patikrina ar pavyko
+        if ($pavyko) {                  // jei pavyko, prideda katasuni
             $katasuniai++;
-            break;
+            break;                      // nutraukia 8 eilute
         }
     }
 }
