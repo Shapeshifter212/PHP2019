6a81a52fe5debb8c0569a11953407bb47bbdbb7d
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-11-07 17:31:42 +0200

    8array

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
-<?php // 1-red 2-green 3-yellow 4-blank
+<?php // 1-red 2-green 3-yellow 
 
 $array = [
-    [2, 2, 2, 2, 2, 2, 2, 2],
-    [2, 3, 3, 3, 3, 3, 3, 2],
-    [2, 3, 1, 1, 1, 1, 3, 2],
-    [2, 3, 1, 3, 3, 1, 3, 2],
-    [2, 3, 1, 3, 3, 1, 3, 2],
-    [2, 3, 1, 1, 1, 1, 3, 2],
-    [2, 3, 3, 3, 3, 3, 3, 2],
-    [2, 2, 2, 2, 2, 2, 2, 2],
+    'hands' => [
+        'left' => 'gin_bottle',
+        'right' => 'vodka_bottle'
+    ],
+    'legs' => 'russian_pre_dance',
+    'head' => 'panda_ultimate_pokerface',
+    'condition' => 'wasted',
 ];
 var_dump($array);
