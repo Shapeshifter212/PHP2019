cc7b993e0965387303a86e6d0b6081f0600f390c
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-11-07 17:14:04 +0200

    6array

--- index.php
+++ index.php
@@ -1,29 +1,4 @@
 <?php
 
-$array = [
-    [
-        'kryptis' => 'desine',
-        'kampas' => 90
-    ],
-    [
-        'kryptis' => 'kaire',
-        'kampas' => 130
-    ],
-    [
-        'kryptis' => 'desine',
-        'kampas' => 70
-    ],
-    [
-        'kryptis' => 'kaire',
-        'kampas' => 120
-    ],
-    [
-        'kryptis' => 'desine',
-        'kampas' => 170
-    ],
-    [
-        'kryptis' => 'desine',
-        'kampas' => 120
-    ],
-];
+$array = [90, -130, 170, -120, 170, 120];
 var_dump($array);
