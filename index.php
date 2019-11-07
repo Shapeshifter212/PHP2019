1bb17ea4ac26fda179140e22db712563a8cd1697
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-11-07 17:08:31 +0200

    5array

--- index.php
+++ index.php
@@ -3,27 +3,27 @@
 $array = [
     [
         'kryptis' => 'desine',
-        'kampas' => '90'
+        'kampas' => 90
     ],
     [
         'kryptis' => 'kaire',
-        'kampas' => '130'
+        'kampas' => 130
     ],
     [
         'kryptis' => 'desine',
-        'kampas' => '70'
+        'kampas' => 70
     ],
     [
         'kryptis' => 'kaire',
-        'kampas' => '120'
+        'kampas' => 120
     ],
     [
         'kryptis' => 'desine',
-        'kampas' => '170'
+        'kampas' => 170
     ],
     [
         'kryptis' => 'desine',
-        'kampas' => '120'
+        'kampas' => 120
     ],
 ];
 var_dump($array);
