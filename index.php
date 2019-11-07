cbe1e0c523a38abfd00e72534a4d07213d0de9d6
Shapeshifter212 <jkavaliauskas@gmail.com>
2019-11-07 16:19:50 +0200

    3array

--- index.php
+++ index.php
@@ -1,56 +1,40 @@
 <?php
 
 $array = [
-    'lentyna' => [
-        'item' => [
-            [
-                'type' => 'kiausiniai',
-                'amount' => '3'
-            ]
+    [
+        [
+            'type' => 'kiausiniai',
+            'amount' => '3'
         ],
-        'item' => [
-            [
-                'type' => 'grietines',
-                'amount' => '1'
-            ]
+        [
+            'type' => 'grietines',
+            'amount' => '1'
         ],
-        'item' => [
-            [
-                'type' => 'tjogurtas',
-                'amount' => '2'
-            ]
+        [
+            'type' => 'jogurtas',
+            'amount' => '2'
         ],
-        'item' => [
-            [
-                'type' => 'zuvis',
-                'amount' => '0'
-            ]
+        [
+            'type' => 'zuvis',
+            'amount' => '0'
         ],
     ],
-    'lentyna' => [
-        'item' => [
-            [
-                'type' => 'pienas',
-                'amount' => '1'
-            ]
+    [
+        [
+            'type' => 'pienas',
+            'amount' => '1'
         ],
-        'item' => [
-            [
-                'type' => 'kefyras',
-                'amount' => '0'
-            ]
+        [
+            'type' => 'kefyras',
+            'amount' => '0'
         ],
-        'item' => [
-            [
-                'type' => 'degtine',
-                'amount' => '2'
-            ]
+        [
+            'type' => 'degtine',
+            'amount' => '2'
         ],
-        'item' => [
-            [
-                'type' => 'kecupas',
-                'amoint' => '2'
-            ]
+        [
+            'type' => 'kecupas',
+            'amoint' => '2'
         ],
     ],
 ];
